95974524b0fa1f1de8b51db754799f5008e37b21
lyno <qdehouse@qq.com>
2017-08-03 17:20:26 +0800

    The fangPlayer example demonstrates how to use fangfis

---
+++ fangPlayer/dev/js/modules/addTitle.js
@@ -0,0 +1,20 @@
+// addTitle.js
+define('modules/addTitle', [
+    'jquery'
+], function (require) {
+    'use strict';
+    console.log('modules/addTitle');
+    let $ = require('jquery');
+    let util = {
+        title: function (cls, txt) {
+            $('body').prepend(`<h2 class="${cls}">${txt}</h2>`);
+        },
+        copyRight: function (txt) {
+            $('body').append(`<p class="copyright">${txt}</p>`);
+        },
+        append: function (txt) {
+            $('body').append(`<span>${txt}</span>`);
+        }
+    };
+    return util;
+});
